8cb29f37898d339a0ef601f5424456b281e6115f
Ankush2107 <ankush.kumar210701@gmail.com>
2023-09-19 15:44:41 +0530

    Creating Project On PhotoSharing app

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const port = 8080;
 
 app.use(express.static('public'))
 
+
+app.get('/', (request, response) => {
+    response.json({
+        "Hosting":  "Kinsta"
+    })
+})
+
 app.listen(port, () => {
     console.log(`Seeing photo app on http://localhost:${port}`);
 })
